965e071a203eead535c0e77156ff163ddd89bf28
Rajesh Reddy Thiyyagura <raj30498@gmail.com>
2023-02-17 15:57:28 -0500

    workflow update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload, finally deployed woohoo!!!
+           finally deployed woohoo!!! with github actions
         </p>
         <a
           className="App-link"
