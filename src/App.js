ed1946b272d6d51d8e0ac459d47fe3ef25e35ddd
Rajesh Reddy Thiyyagura <39207903+rajeshreddy-T@users.noreply.github.com>
2023-05-03 20:55:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,16 +7,9 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-           finally deployed woohoo!!! with github actions Hey class how is it going
+         Frontend deployed to availability zone - b in us-east-1
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
